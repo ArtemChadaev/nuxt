05ea072eba39ca85df8311892aae047bcbf90d80
ChadaevArtem <1chadaevartem030806@gmail.com>
2025-08-21 20:32:59 +0300

    21 08 25

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,23 @@
-<!-- eslint-disable vue/multi-word-component-names -->
-<script setup></script>
+<script setup lang="ts">
+import BodySection from "~/pages/components/BodySection.vue";
+</script>
 <template>
-	<div class="p-10 h-screen w-screen">
-		<p class="border h-10 w-10">1</p>
-	</div>
+  <div
+    class="h-screen w-full grid grid-rows-[5%_95%] bg-neutral-50 justify-center"
+  >
+    <div
+      class="grid 2xl:grid-cols-[1fr_1400px_1fr] xl:grid-cols-[1fr_1200px_1fr] lg:grid-cols-[1fr_900px_1fr] md:grid-cols-[1fr_600px_1fr] pt-2"
+    >
+      <div />
+      <AppHeader />
+      <div />
+    </div>
+    <div
+      class="grid 2xl:grid-cols-[1fr_1200px_1fr] xl:grid-cols-[1fr_1000px_1fr] lg:grid-cols-[1fr_800px_1fr] md:grid-cols-[1fr_500px_1fr]"
+    >
+      <div />
+      <BodySection />
+      <div />
+    </div>
+  </div>
 </template>
-<style lang="postcss" scoped></style>
